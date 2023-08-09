599f048f23b8725546194b8741bf84be5a573dff
= <=>
2023-08-09 19:13:05 -0300

    creacion de carpeta countdown

---
+++ countdown/main.js
@@ -0,0 +1,6 @@
+const days = document.querySelector('#days');
+const hours = document.querySelector('#hours');
+const mins = document.querySelector('#mins');
+const secs = document.querySelector('#secs');
+
+
