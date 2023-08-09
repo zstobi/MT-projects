dcc12b57b6b904113bd5a99e147ff17f771b4c50
= <=>
2023-08-09 19:35:41 -0300

    numbers and logic started

--- countdown/main.js
+++ countdown/main.js
@@ -1,3 +1,4 @@
+const months = document.querySelector('#months');
 const days = document.querySelector('#days');
 const hours = document.querySelector('#hours');
 const mins = document.querySelector('#mins');
@@ -5,3 +6,17 @@ const secs = document.querySelector('#secs');
 
 const day = new Date();
 days.textContent = day.getDate();
+
+const month = new Date();
+months.textContent = day.getMonth() + 1;
+
+const hour = new Date();
+hours.textContent = day.getHours();
+
+const min = new Date();
+mins.textContent = day.getMinutes();
+
+setInterval(()=>{
+    let sec = new Date();
+    secs.textContent = day.getSeconds();
+},1000);
