fcabb27c56985a61406fd9c796b9e1c955efd92b
rockito10 <107514701+rockito10@users.noreply.github.com>
2023-08-09 20:17:20 -0300

    working clock

--- countdown/main.js
+++ countdown/main.js
@@ -1,17 +1,26 @@
+// constantes del reloj
 const months = document.querySelector('#months');
 const days = document.querySelector('#days');
 const hours = document.querySelector('#hours');
 const mins = document.querySelector('#mins');
 const secs = document.querySelector('#secs');
 
-const date = new Date();
 
-days.textContent = date.getDate();
-months.textContent = date.getMonth() + 1;
-hours.textContent = date.getHours();
-mins.textContent = date.getMinutes();
+// intervalos / contadores
 
 setInterval(()=>{
-    let second = new Date();
-    secs.textContent = second.getSeconds();
+    let date = new Date();
+    secs.textContent = date.getSeconds(); 
+    mins.textContent = date.getMinutes(); 
+    hours.textContent = date.getHours();
+    days.textContent = date.getDate(); // 21
+    months.textContent = date.getMonth() + 1; // 9
+
+    /*
+    Este intervalo cada 1 seg obtiene la fecha actual vía objeto Date().
+    luego utiliza la fecha actual para actualizar los datos constantemente.
+    */
+
 },1000);
+
+console.log(Date())
