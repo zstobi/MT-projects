1967757dfedee8c2886f3b56d1ce848c0b09458d
= <=>
2023-08-09 19:22:18 -0300

    day logic coding

--- countdown/main.js
+++ countdown/main.js
@@ -3,4 +3,5 @@ const hours = document.querySelector('#hours');
 const mins = document.querySelector('#mins');
 const secs = document.querySelector('#secs');
 
-
+const day = new Date();
+days.textContent = day.getDate();
