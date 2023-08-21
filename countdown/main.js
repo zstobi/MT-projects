a724bdc33156e3ae926294742155368ef94abc27
= <=>
2023-08-21 01:17:47 -0300

    new img interval created

--- countdown/main.js
+++ countdown/main.js
@@ -44,9 +44,10 @@ let distanceToWishedDate = ( meses[currentMonth] - currentDate.getDate() ) + dat
 // intervalos / contadores / inicialización del contador final
 
 let dateUntilRelease = new Date();
-dateUntilRelease.setSeconds(60 - dateUntilRelease.getSeconds())
-dateUntilRelease.setMinutes(60 - dateUntilRelease.getMinutes())
-dateUntilRelease.setHours(23 - dateUntilRelease.getHours())
+dateUntilRelease.setSeconds(59 - dateUntilRelease.getSeconds());
+console.log(dateUntilRelease.getSeconds())
+dateUntilRelease.setMinutes(60 - dateUntilRelease.getMinutes());
+dateUntilRelease.setHours(23 - dateUntilRelease.getHours());
 
 //date2 es la fecha de hoy
 
@@ -58,7 +59,7 @@ setInterval(()=>{
     let restaMesesFechaActualConDeseada = 7 - actualDate.getMonth();
     dateUntilRelease.setDate(restaDiasFechaActualConDeseada);
     dateUntilRelease.setMonth(restaMesesFechaActualConDeseada);
-    dateUntilRelease.setSeconds(dateUntilRelease.getSeconds() - 1)
+    dateUntilRelease.setSeconds(dateUntilRelease.getSeconds() - 1);
 
     let remainingMonth = dateUntilRelease.getMonth() + 1;
     let remainingSeconds = dateUntilRelease.getSeconds();
@@ -109,25 +110,63 @@ setInterval(()=>{
 // contenedor de imagenes
 const imgContainer = document.querySelector('.imgBorder');
 // lista de imagenes
-let imgList = [
-    'https://cdn.cloudflare.steamstatic.com/steam/apps/1272080/ss_9c46b95ab12abc2f78e4e422b74f7606d6babc19.1920x1080.jpg?t=1691159104',
-    'https://www.infobae.com/new-resizer/oIXP1_Txc0wzutDasLESlYkh4mY=/arc-anglerfish-arc2-prod-infobae/public/C3OB372WGJEXFB2I4RP442PQRE.jpg',
-    'https://prod.assets.earlygamecdn.com/images/Payday-3-Release-Date-Gameplay-Combat-Characters.png?mtime=1688657051',
-    'https://gaming-cdn.com/images/products/6442/616x353/payday-3-pc-spil-steam-cover.jpg?v=1686753600',
-    'https://www.somosxbox.com/wp-content/uploads/2023/05/payday-3-1.jpg'
-];
+// let imgList = [
+//     'https://cdn.cloudflare.steamstatic.com/steam/apps/1272080/ss_9c46b95ab12abc2f78e4e422b74f7606d6babc19.1920x1080.jpg?t=1691159104',
+//     'https://www.infobae.com/new-resizer/oIXP1_Txc0wzutDasLESlYkh4mY=/arc-anglerfish-arc2-prod-infobae/public/C3OB372WGJEXFB2I4RP442PQRE.jpg',
+//     'https://prod.assets.earlygamecdn.com/images/Payday-3-Release-Date-Gameplay-Combat-Characters.png?mtime=1688657051',
+//     'https://gaming-cdn.com/images/products/6442/616x353/payday-3-pc-spil-steam-cover.jpg?v=1686753600',
+//     'https://www.somosxbox.com/wp-content/uploads/2023/05/payday-3-1.jpg'
+// ];
 
-// contador de imgs
+// // contador de imgs
+// let imgCounter = 0;
+
+// setInterval(()=>{
+
+//     if (imgCounter === 4){
+//         imgContainer.innerHTML = `<img src="${imgList[imgCounter]}" alt="">`;
+//         imgCounter = 0;
+//     } else {
+//         imgContainer.innerHTML = `<img src="${imgList[imgCounter]}" alt="">`;
+//         imgCounter+=1;
+//     }
+
+// },5000);
+
+let img1 = document.querySelector('#img1');
+let img2 = document.querySelector('#img2');
+let img3 = document.querySelector('#img3');
+let img4 = document.querySelector('#img4');
+let img5 = document.querySelector('#img5');
+
+let imgList = [img1,img2,img3,img4,img5];
+
 let imgCounter = 0;
 
-setInterval(()=>{
+// function 
 
-    if (imgCounter === 4){
-        imgContainer.innerHTML = `<img src="${imgList[imgCounter]}" alt="">`;
-        imgCounter = 0;
+setInterval(()=>{
+    imgCounter++;
+
+    if (imgCounter !== 5 && imgList[imgCounter].classList.contains('displayOff')){
+        // efectuar cambio de display en imagen previa
+        imgList[imgCounter -1].classList.remove('displayOn');
+        imgList[imgCounter -1].classList.add('displayOff');
+
+        // efectuar cambio de display en imagen actual
+        imgList[imgCounter].classList.remove('displayOff');
+        imgList[imgCounter].classList.add('displayOn');
+
     } else {
-        imgContainer.innerHTML = `<img src="${imgList[imgCounter]}" alt="">`;
-        imgCounter+=1;
+        // efectuar cambio de display en imagen previa
+        imgList[imgCounter -1].classList.remove('displayOn');
+        imgList[imgCounter -1].classList.add('displayOff');
+
+        imgCounter = 0;
+        
+        // efectuar cambio de display en imagen actual
+        imgList[imgCounter].classList.remove('displayOff');
+        imgList[imgCounter].classList.add('displayOn');
     }
 
 },5000);
